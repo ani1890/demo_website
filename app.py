b058bc7b4a6ef81a0dd0d8e6d57d8c86ca30b9a5
ani1890 <142164897+ani1890@users.noreply.github.com>
2025-03-24 22:19:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,22 +14,31 @@ num3 = st.number_input("3rd number : ", value=None, step=1)
 
 but = st.button("Addition")
 if but:
+  sum = num1 + num2 + num3
+  st.write(num1, "+", num2, "+", num3, "=", sum)
   st.write("Addition of all 3 numbers is: ", num1 + num2 + num3)
+  st.markdown("![Alt Text](https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjMyNXZwMzloYjRwdnN5bzVscXJmcDVyMTJiOHgwc24ya3RoN2loYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/MelhioWPAo6k4Q6BTp/giphy.gif)")
   
 but1 = st.button("Substraction")
 if but1:
   sub1 = num1-num2-num3
   sub2 = num2-num3-num1
-  sub3 = num3-num2-num1
+  sub3 = num3-num1-num2
   
   if sub1 >= 0:
+    st.write(num1, "-", num2, "-", num3, "=", sub1)
     st.write("Substraction of all 3 numbers is: ", sub1)
+    st.markdown("![Alt Text](https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjMyNXZwMzloYjRwdnN5bzVscXJmcDVyMTJiOHgwc24ya3RoN2loYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/MelhioWPAo6k4Q6BTp/giphy.gif)")
   
   elif sub2 >= 0:
+    st.write(num2, "-", num3, "-", num1, "=", sub2)
     st.write("Substraction of all 3 numbers is: ", sub2)
+    st.markdown("![Alt Text](https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjMyNXZwMzloYjRwdnN5bzVscXJmcDVyMTJiOHgwc24ya3RoN2loYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/MelhioWPAo6k4Q6BTp/giphy.gif)")
     
   elif sub3 >= 0:
+    st.write(num3, "-", num1, "-", num2, "=", sub3)
     st.write("Substraction of all 3 numbers is: ", sub3)
+    st.markdown("![Alt Text](https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjMyNXZwMzloYjRwdnN5bzVscXJmcDVyMTJiOHgwc24ya3RoN2loYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/MelhioWPAo6k4Q6BTp/giphy.gif)")
     
   else:
     st.write("Result is in negative figure!!!")
@@ -39,5 +48,10 @@ if but1:
   
 but2 = st.button("Multiplication")
 if but2:
-  st.write("Multiplication of all 3 numbers is: ", num1 * num2 * num3)
+  mul = num1 * num2 * num3
+  st.write(num1, "X", num2, "X", num3, "=", mul)
+  st.write("Multiplication of all 3 numbers is: ", mul)
+  st.markdown("![Alt Text](https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjMyNXZwMzloYjRwdnN5bzVscXJmcDVyMTJiOHgwc24ya3RoN2loYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/MelhioWPAo6k4Q6BTp/giphy.gif)")
+  
+
   
