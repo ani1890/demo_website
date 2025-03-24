ba1dcadfc8ad4f785d9c56b11ce49366561e9053
ani1890 <142164897+ani1890@users.noreply.github.com>
2025-03-24 20:57:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import streamlit as st
 import time
 
-name = st.text_input("Enter your full Name :")
-age = st.number_input("Age : ", value=None, step=1)
-city = st.selectbox("Choose city", ("Mumbai", "Pune", "Bangalore"))
-pin = st.number_input("Enter your pincode :", value=None, step=1)
+# name = st.text_input("Enter your full Name :")
+# age = st.number_input("Age : ", value=None, step=1)
+# city = st.selectbox("Choose city", ("Mumbai", "Pune", "Bangalore"))
+# pin = st.number_input("Enter your pincode :", value=None, step=1)
 
-but = st.button("Submit")
+num1 = st.number_input("1st number : ", value=None, step=1)
+num2 = st.number_input("2nd number : ", value=None, step=1)
+num3 = st.number_input("3rd number : ", value=None, step=1)
+
+
+
+but = st.button("Addition")
 if but:
-  st.markdown(f""" 
-  Name : {name}\n
-  Age : {age}\n
-  City : {city}\n
-  Pincode : {pin}           
-              """)
+  st.write("Addition of all 3 numbers is: ", num1 + num2 + num3)
+  
+but1 = st.button("Substraction")
+if but1:
+  st.write("Substraction of all 3 numbers is: ", num1 - num2 - num3)
+  
+but2 = st.button("Multiplication")
+if but2:
+  st.write("Multiplication of all 3 numbers is: ", num1 * num2 * num3)
